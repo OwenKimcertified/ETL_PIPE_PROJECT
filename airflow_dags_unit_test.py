0059253719555700a6efbeafdeb3cb6a15ec0a8f
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2023-09-12 11:55:46 +0900

    Update airflow_dags_unit_test.py

--- airflow_dags_unit_test.py
+++ airflow_dags_unit_test.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from api_ETL_PIPELINE import dag
 
 def test_dag_loading():
-    dagbag = DagBag(include_examples=False)
+    dagbag = DagBag(include_examples = False)
     assert len(dagbag.dags) == 1, "Number of DAGs is different than expected."
 
 def test_etl_datapipeline():
@@ -16,5 +16,5 @@ def test_etl_datapipeline():
 
     # Check the tasks included in the DAG
     task_ids = [task.task_id for task in dag.tasks]
-    expected_task_ids = ['available_or_not', 'extract_riot_api', 'process_api_data']
+    expected_task_ids = ['available_or_not', 'extract_riot_api', 'process_api_data', 'kafka_logging_pipeline']
     assert all(task_id in task_ids for task_id in expected_task_ids), "Required tasks are missing from the DAG."
